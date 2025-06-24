a272e7c679edd55fbd2f66ab49f83b36a497f8bf
NirmitJain <nirmit.jain@gmail.com>
2025-06-24 15:00:37 +0530

    Section 1 changes
    section 1 - basic spring security

---
+++ section1/springsecuritysection1/src/main/java/com/eazybytes/springsecuritysection1/EazyBankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.eazybytes.springsecuritysection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("com.eazybtes.springsecuritysection1.controller")
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
