0070f3a52ce453500c00a9e08c677d6e1eb2a451
NirmitJain <nirmit.jain@gmail.com>
2025-07-03 13:28:21 +0530

    section 3
    Defining & managing user using InMemoryUserDetailManager

--- section3/springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section3/springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,10 +2,19 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -15,31 +24,37 @@ public class ProjectSecurityConfig {
 //                ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
         http.authorizeHttpRequests(requests ->
                 requests
-                        .requestMatchers("/userAccount","/userBalance","/userLoan","/userCards")
-                        .authenticated().requestMatchers("/notices","/contacts","/error","/welcome").permitAll());
+                        .requestMatchers("/userAccount", "/userBalance", "/userLoan", "/userCards")
+                        .authenticated().requestMatchers("/notices", "/contacts", "/error", "/welcome").permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         // to disable formlogin and basic authentication
        /* http.formLogin(flc->flc.disable());
         http.httpBasic(hbc->hbc.disable());*/
 
-        return (SecurityFilterChain)http.build();
+        return (SecurityFilterChain) http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
 
-       UserDetails user = User.withUsername("user")
-        .password(passwordEncoder().encode("{noop}12345"))
-        .authorities("read").build();
+        UserDetails user = User.withUsername("user").password("eazybytes@12345")
+                .authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$7MTj.SOehOppYuufpNgXUeyH54d7c4K/8C239.LC7Il10kDKnw6FO")
+                .authorities("admin").build();
 
-       UserDetails admin = User.withUsername("admin")
-        .password(passwordEncoder().encode("{noop}54321"))
-        .authorities("admin").build();
-
-    return new InmemoryUserDetailsManager(user,admin);
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
 
